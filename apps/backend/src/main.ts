683348d5bc06d2b2ecae8e1904cbef8aff0c6172
TheCommandCat <bhrtkhvji@gmail.com>
2025-01-20 22:38:03 +0200

    init backend + imported base code

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cookies from 'cookie-parser';
+import cors from 'cors';
+import authRouter from './routers/auth';
+import * as path from 'path';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const app = express();
+const corsOptions = {
+  origin: [/localhost:\d+$/],
+  credentials: true
+};
+
+app.use(cookies());
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// app.use('/auth', authRouter);
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
