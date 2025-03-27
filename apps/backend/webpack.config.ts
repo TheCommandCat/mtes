75dd185f272f1d012c923089aa1f28300608c586
Nir Chen <bhrtkhvji@gmail.com>
2025-03-28 01:03:58 +0200

    Revert "Turned config files from js to ts for consistency"
    This reverts commit dcccea5309a0eb49f21202312ffc093e3e52820a.

--- apps/backend/webpack.config.ts
+++
@@ -1,8 +0,0 @@
-import { composePlugins, withNx } from '@nx/webpack';
-import type { Configuration } from 'webpack';
-
-export default composePlugins(withNx(), (config: Configuration) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins?.push(new MyPlugin())`
-  return config;
-});
