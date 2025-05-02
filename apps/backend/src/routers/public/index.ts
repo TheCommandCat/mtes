1f3dbc6cf76515bb717dabc561cfbf1bf5dc990a
Nir Chen <bhrtkhvji@gmail.com>
2025-05-02 03:44:53 +0300

    Removed unused event route

--- apps/backend/src/routers/public/index.ts
+++ apps/backend/src/routers/public/index.ts
@@ -3,24 +3,10 @@ import * as db from '@mtes/database';
 
 const router = express.Router({ mergeParams: true });
 
-router.get('/events', (req: Request, res: Response) => {
-  db.getAllElectionEvents().then(events => {
-    res.json(
-      events.map(event => {
-        return event;
-      })
-    );
-  });
-});
-
 router.get('/event', (req: Request, res: Response) => {
   db.getElectionEvent().then(event => {
     res.status(200).json(event);
   });
 });
-
-// router.use('/divisions', divisionsRouter);
-
-// router.use('/portal', portalRouter);
 
 export default router;
