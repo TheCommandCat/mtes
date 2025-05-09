8c016df49ec299b7106112465027fde5282d3926
Nir Chen <bhrtkhvji@gmail.com>
2025-05-09 18:30:34 +0300

    fixed ts warning

--- libs/types/src/lib/voting-states.ts
+++ libs/types/src/lib/voting-states.ts
@@ -1,8 +1,9 @@
+import { WithId } from 'mongodb';
 import { Member } from './schemas/member';
 
 export const STAND_STATES = ['NotStarted', 'Voting', 'VotingSubmitted', 'Empty'] as const;
 export type VotingStates = (typeof STAND_STATES)[number];
 export interface VotingStandStatus {
   status: VotingStates;
-  member: Member | null;
+  member: WithId<Member> | null;
 }
