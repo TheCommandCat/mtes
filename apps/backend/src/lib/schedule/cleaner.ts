9ba37edc8bfd18ddb94658bbbe9ceaab5f59a131
Nir Chen <bhrtkhvji@gmail.com>
2025-05-22 00:42:26 +0300

    Refactors cleanDivisionData function to properly await addUser and handle errors

--- apps/backend/src/lib/schedule/cleaner.ts
+++ apps/backend/src/lib/schedule/cleaner.ts
@@ -3,16 +3,15 @@ import * as db from '@mtes/database';
 export const cleanDivisionData = async () => {
   if (!(await db.deleteElectionEvent())) throw new Error('Could not delete event!');
   if (!(await db.deleteUsers({})).acknowledged) throw new Error('Could not delete users!');
-  db.addUser({
+  const result = await db.addUser({
     username: 'admin',
     isAdmin: true,
     password: 'admin',
     lastPasswordSetDate: new Date()
-  }).then(result => {
-    if (!result.acknowledged) {
-      throw new Error('Could not add admin user!');
-    }
   });
+  if (!result.acknowledged) {
+    throw new Error('Could not add admin user!');
+  }
   if (!(await db.deleteElectionState()).acknowledged) {
     throw new Error('Could not delete Election state!');
   }
