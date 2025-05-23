5aaf61cf7a3b8a9bc1e0f817d61c3aead6224ae2
Nir Chen <bhrtkhvji@gmail.com>
2025-05-23 19:15:04 +0300

    Add additional positions to the Position constant

--- libs/types/src/lib/positions.ts
+++ libs/types/src/lib/positions.ts
@@ -1,2 +1,10 @@
-export const Position = ['יו"ר', 'סיו"ר', 'מזכ"ל'] as const;
+export const Position = [
+  'יו"ר',
+  'סיו"ר',
+  'מזכ"ל',
+  'דובר.ת',
+  'יו"ר הוועדה האתית משפטית',
+  'יו"ר וועדת נו"ק',
+  'מבקר.ת'
+] as const;
 export type Positions = (typeof Position)[number];
