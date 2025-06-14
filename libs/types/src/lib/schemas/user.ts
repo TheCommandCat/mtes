5c5e935e7d5919eea456c34fc1ca3d28865228cb
Nir Chen <bhrtkhvji@gmail.com>
2025-06-14 22:48:33 +0300

    Reverted breaking change in user type

--- libs/types/src/lib/schemas/user.ts
+++ libs/types/src/lib/schemas/user.ts
@@ -3,13 +3,13 @@ import { Role } from '../roles';
 export interface User {
   username?: string;
   isAdmin: boolean;
-  role: Role;
+  role?: Role;
   password: string;
   roleAssociation?: {
     type: 'stand';
     value: number;
   };
-  lastPasswordSetDate?: Date;
+  lastPasswordSetDate: Date;
 }
 
 export type SafeUser = Omit<User, 'password' | 'lastPasswordSetDate'>;
